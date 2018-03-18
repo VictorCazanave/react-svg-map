9b5f03424751dab73b73efadd4d48f23304d0e6f
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-18 19:45:07 +0800

    [Webpack] Add resolve extensions

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+	entry: './example/index.jsx',
 	module: {
 		rules: [{
 				test: /\.jsx?/,
@@ -20,9 +21,12 @@ module.exports = {
 			}
 		]
 	},
+	resolve: {
+		extensions: ['.json', '.js', '.jsx'],
+	},
 	plugins: [
 		new HtmlWebPackPlugin({
-			template: './src/index.html',
+			template: './example/index.html',
 			filename: './index.html'
 		})
 	]
