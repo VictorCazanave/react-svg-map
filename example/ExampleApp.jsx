893bd110286ece57003fbd22ca7436b8d6306c14
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-18 19:45:37 +0800

    [Example] Create basic example app

---
+++ example/ExampleApp.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ExampleApp extends React.Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	render() {
+
+		return (
+			<section>
+				<h1>Example of react-taiwan-map use</h1>
+				<div>
+				</div>
+			</section>
+		);
+	}
+}
+
+export default ExampleApp;
---
+++ example/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleApp from './ExampleApp';
+
+ReactDOM.render(<ExampleApp />, document.getElementById('root'));
