f3e08af9cfbd1e3194b14b987f72c51edeba3fb1
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-21 21:59:04 +0800

    [TaiwanMap] Handle default params

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
-import TaiwanMapLocations from './TaiwanMapLocations';
+import TaiwanMapSVG from './TaiwanMapSVG';
 import './index.scss';
 
 function TaiwanMap(props) {
 	// Set default props
-	const locations = props.locations || TaiwanMapLocations;
-	const tabIndex = props.locationTabIndex || '0';
-	const role = props.locationRole || 'button';
+	const svg = props.svg || TaiwanMapSVG;
+	const tabIndex = props.tabIndex || '0'; // Make it focusable
+	const role = props.type || 'none';
+
+	// Warn message when isLocationSelected function is not provided
+	if ((role === 'checkbox' || role === 'radio') && !(props.isLocationSelected instanceof Function)) {
+		console.warn('No isLocationSelected function provided.');
+	}
 
 	return (
-		<svg className="taiwan-map" xmlns="http://www.w3.org/2000/svg" viewBox="312 322 688 973" aria-label="Map of Taiwan">
+		<svg className="taiwan-map" xmlns="http://www.w3.org/2000/svg" viewBox={svg.viewBox} role="group" aria-label={svg.label}>
 		{
-			locations.map(location =>
-				(<path
-					id={location.id}
-					className="taiwan-map__location"
-					name={location.name}
-					d={location.path}
-					onMouseOver={props.onLocationMouseOver}
-					onMouseOut={props.onLocationMouseOut}
-					onClick={props.onLocationClick}
-					onFocus={props.onLocationFocus}
-					onBlur={props.onLocationBlur}
-					role={role}
-					tabIndex={tabIndex}
-					aria-label={location.name}
-					aria-selected={props.isLocationSelected(location)}
-					key={location.id} />
-				)
+			svg.locations.map(location => {
+				return (<path
+						id={location.id}
+						className="taiwan-map__location"
+						name={location.name}
+						d={location.path}
+						onMouseOver={props.onLocationMouseOver}
+						onMouseOut={props.onLocationMouseOut}
+						onClick={props.onLocationClick}
+						onFocus={props.onLocationFocus}
+						onBlur={props.onLocationBlur}
+						tabIndex={tabIndex}
+						role={role}
+						aria-label={location.name}
+						aria-checked={props.isLocationSelected && props.isLocationSelected(location)}
+						key={location.id} />);
+				}
 			)
 		}
 	</svg>
