1d9ac326adbe7b936fc6bfa1936699a06d811d2a
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-22 11:08:03 +0800

    [SVGMap] Remove scss import and default map value

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import TaiwanMap from './maps/taiwan';
-import './index.scss';
 
 function SVGMap(props) {
 	// Set default props
-	const map = props.map || TaiwanMap;
 	const tabIndex = props.tabIndex || '0'; // Make it focusable
 	const role = props.type || 'none';
 
-	// Warn message when isLocationSelected function is not provided
+	// Warn message when needed function are not provided
 	if ((role === 'checkbox' || role === 'radio') && !(props.isLocationSelected instanceof Function)) {
 		console.warn('No isLocationSelected function provided.');
+	} else if (role === 'link' && !(props.onLocationClick instanceof Function)) {
+		console.warn('No onLocationClick function provided.');
 	}
 
 	return (
-		<svg className="svg-map" xmlns="http://www.w3.org/2000/svg" viewBox={map.viewBox} role="group" aria-label={map.label}>
+		<svg className="svg-map" xmlns="http://www.w3.org/2000/svg" viewBox={props.map.viewBox} role="group" aria-label={props.map.label}>
 		{
-			map.locations.map(location => {
+			props.map.locations.map(location => {
 				return (<path
 						id={location.id}
 						className="svg-map__location"
