272d6c119fb8543def9b473f7d2d895f26640da5
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-25 17:51:34 +0800

    [Test] Update snapshots

--- test/svg-map.spec.jsx
+++ test/svg-map.spec.jsx
@@ -3,8 +3,30 @@ import renderer from 'react-test-renderer';
 import SVGMap from '../src/svg-map';
 import Taiwan from '../src/maps/taiwan';
 
-test('SVGMap displays map of Taiwan', () => {
+test('SVGMap displays map of Taiwan with default props', () => {
 	const component = renderer.create(<SVGMap map={Taiwan}/>);
-	let tree = component.toJSON();
+	const tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+test('SVGMap displays map of Taiwan with custom props', () => {
+	const isSelected = () => false;
+	const component = renderer.create(<SVGMap map={Taiwan} type="checkbox" tabIndex="1" isLocationSelected={isSelected} />);
+	const tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+test('SVGMap displays custom map', () => {
+	const customMap = {
+		label: 'custom map',
+		viewBox: 'custom view box',
+		locations: [{
+			name: 'custom name',
+			id: 'custom id',
+			path: 'custom path'
+		}]
+	};
+	const component = renderer.create(<SVGMap map={customMap}/>);
+	const tree = component.toJSON();
 	expect(tree).toMatchSnapshot();
 });
