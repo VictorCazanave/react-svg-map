e5ae9f5a078ac4a31a35155c921db5da8cc33f58
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-28 21:09:47 +0800

    [Tests] Add and update tests

--- __tests__/svg-map.test.jsx
+++ __tests__/svg-map.test.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { SVGMap, Taiwan } from '../src/';
+import { SVGMap, Australia, France, Taiwan } from '../src/';
+
+test('SVGMap displays map of Australia with default props', () => {
+	const component = renderer.create(<SVGMap map={Australia}/>);
+	const tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+test('SVGMap displays map of Australia with custom props', () => {
+	const isSelected = () => false;
+	const component = renderer.create(<SVGMap map={Australia} type="radio" tabIndex="1" isLocationSelected={isSelected} />);
+	const tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+test('SVGMap displays map of France with default props', () => {
+	const component = renderer.create(<SVGMap map={France}/>);
+	const tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
+
+test('SVGMap displays map of France with custom props', () => {
+	const component = renderer.create(<SVGMap map={France} type="link" tabIndex="2" />);
+	const tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
 
 test('SVGMap displays map of Taiwan with default props', () => {
 	const component = renderer.create(<SVGMap map={Taiwan}/>);
@@ -9,8 +34,8 @@ test('SVGMap displays map of Taiwan with default props', () => {
 });
 
 test('SVGMap displays map of Taiwan with custom props', () => {
-	const isSelected = () => false;
-	const component = renderer.create(<SVGMap map={Taiwan} type="checkbox" tabIndex="1" isLocationSelected={isSelected} />);
+	const isSelected = () => true;
+	const component = renderer.create(<SVGMap map={Taiwan} type="checkbox" tabIndex="3" isLocationSelected={isSelected} />);
 	const tree = component.toJSON();
 	expect(tree).toMatchSnapshot();
 });
