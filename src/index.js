32714516f50059fc21481ddc176223a479bc96b8
Victor Cazanave <victor.cazanave@gmail.com>
2018-12-08 22:59:12 +0800

    [Maps] Add SVG maps and delete JS maps

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import SVGMap from './svg-map';
-import Australia from './maps/australia';
-import France from './maps/france';
-import Taiwan from './maps/taiwan';
-import USA from './maps/usa';
-import Utah from './maps/utah';
+import Australia from './maps/australia.svg';
+import France from './maps/france.svg';
+import Taiwan from './maps/taiwan.svg';
+import USA from './maps/usa.svg';
+import Utah from './maps/utah.svg';
 import './svg-map.scss';
 
 export {
