b7b317d0139aa040d1f52ee67f464d4be3c886c9
Ramon Rios <ramosquito5@gmail.com>
2019-04-04 22:39:31 -0600

    creating a snapshot and a test for the heatmap

--- __tests__/svg-map.test.js
+++ __tests__/svg-map.test.js
@@ -45,7 +45,35 @@ describe('SVGMap component', () => {
 
 			expect(tree).toMatchSnapshot();
 		});
+
+		test('displays heat map with custom location css', () => {
+			const eventHandler = () => 'eventHandler';
+			const isLocationSelected = () => 'isLocationSelected';
+			const generateHeat = () => 'heat0';
+			const component = renderer.create(
+				<SVGMap map={map}
+					className="className"
+					role="role"
+					locationClassName="locationClassName"
+					locationTabIndex="locationTabIndex"
+					locationRole="locationRole"
+					onLocationMouseOver={eventHandler}
+					onLocationMouseOut={eventHandler}
+					onLocationMouseMove={eventHandler}
+					onLocationClick={eventHandler}
+					onLocationKeyDown={eventHandler}
+					onLocationFocus={eventHandler}
+					onLocationBlur={eventHandler}
+					isLocationSelected={isLocationSelected}
+					handleLocationClasses={generateHeat}
+				/>
+			);
+			const tree = component.toJSON();
+
+			expect(tree).toMatchSnapshot();
+		});
 	});
+
 
 	describe('Maps', () => {
 		test('displays map of Australia', () => {
