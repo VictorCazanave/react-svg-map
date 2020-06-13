4e29c03235624f0ee82d2e7bff63782345f10d48
Victor Cazanave <victor.cazanave@gmail.com>
2020-06-13 10:44:57 +0800

    Fix missing css file in production

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-  // Fix "localStorage is not available for opaque origins" error
-  // https://github.com/jsdom/jsdom/issues/2304
-  testURL: 'http://localhost',
+	// Fix "localStorage is not available for opaque origins" error
+	// https://github.com/jsdom/jsdom/issues/2304
+	testURL: 'http://localhost',
 
-  setupTestFrameworkScriptFile: '<rootDir>/jest-setup.js',
-  testMatch: ['**/__tests__/**/*.test.js'],
-  collectCoverage: true,
-  coverageDirectory: './coverage/',
-  collectCoverageFrom: [
-    'src/*.jsx'
-  ],
+	// Mock imported scss file
+	// https://jestjs.io/docs/en/webpack.html#handling-static-assets
+	moduleNameMapper: {
+		'\\.scss$': '<rootDir>/__tests__/__mocks__/styleMock.js'
+	},
+
+	setupTestFrameworkScriptFile: '<rootDir>/jest-setup.js',
+	testMatch: ['**/__tests__/**/*.test.js'],
+	collectCoverage: true,
+	coverageDirectory: './coverage/',
+	collectCoverageFrom: [
+		'src/*.jsx'
+	],
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import SVGMap from './svg-map';
 import CheckboxSVGMap from './checkbox-svg-map';
 import RadioSVGMap from './radio-svg-map';
+import './svg-map.scss';
 
 export {
 	SVGMap,
